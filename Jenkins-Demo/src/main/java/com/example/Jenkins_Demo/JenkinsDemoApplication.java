bc40a6379767da75335c1e5bbfb090e3e3eb9d83
Kannan <kannanlearning2621.com>
2025-01-07 11:00:21 +0530

    Jenkins-Project

---
+++ Jenkins-Demo/src/main/java/com/example/Jenkins_Demo/JenkinsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.Jenkins_Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsDemoApplication.class, args);
+	}
+
+}
